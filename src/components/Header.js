db896ac011c6214a3bf67c7983923e735da0dd55
Ben Murray <ben@mediacarry.com>
2022-02-11 00:09:08 +0000

    updating links

--- src/components/Header.js
+++ src/components/Header.js
@@ -44,7 +44,7 @@ export const Header = () => {
                   <img src={image2} alt="No image" />
                 </a>
                 <div className="col-mobile mobile-show">
-                  <a className="btn-mint" href="#">Mint coming soon</a>
+                  <a className="btn-mint" href="https://twitter.com/honeyznft">Mint coming soon</a>
                 </div>
                 <ul className='header-link navbar-nav me-auto mb-2 mb-lg-0'>
                   <HashLink smooth to={'/#home-id'}><li className="nav-item"><a className="nav-link" href="#">Home</a></li></HashLink>
@@ -56,10 +56,10 @@ export const Header = () => {
                   <HashLink smooth to={'/faq#faq-id'}><li className="nav-item"><a className="nav-link" href="#">Faq</a></li></HashLink>
                 </ul>
                 <ul className='social-links mobile-show'>
-                  <li><i className="icon-fa icon-twitter"> </i></li>
-                  <li><i className="icon-fa icon-discord"></i></li>
-                  <li><i className="icon-fa icon-instagram"></i></li>
-                  <li><i className="icon-fa icon-logomark"></i></li>
+                  <li><a href="https://twitter.com/honeyznft"><i className="icon-fa icon-twitter"></i></a></li>
+                  <li><a href="https://discord.gg/pT52wbTFJ7"><i className="icon-fa icon-discord"></i></a></li>
+                  <li><a href="https://instagram.com/honeyznft"><i className="icon-fa icon-instagram"></i></a></li>
+                  <li><a href="#"><i className="icon-fa icon-logomark"></i></a></li>
                 </ul>
                 <div className="placeholder-mobile mobile-show">
                   <img src={image4} alt="No image" />
@@ -68,14 +68,14 @@ export const Header = () => {
             </nav>
           </div>
           <div className="col mobile-show">
-            <a className="btn-mint" href="#">Mint coming soon</a>
+            <a className="btn-mint" href="https://twitter.com/honeyznft">Mint coming soon</a>
           </div>
           <div className="col desktop-show">
             <ul className='social-links'>
-              <li><i className="icon-fa icon-twitter"> </i></li>
-              <li><i className="icon-fa icon-discord"></i></li>
-              <li><i className="icon-fa icon-instagram"></i></li>
-              <li><i className="icon-fa icon-logomark"></i></li>
+                  <li><a href="https://twitter.com/honeyznft"><i className="icon-fa icon-twitter"></i></a></li>
+                  <li><a href="https://discord.gg/pT52wbTFJ7"><i className="icon-fa icon-discord"></i></a></li>
+                  <li><a href="https://instagram.com/honeyznft"><i className="icon-fa icon-instagram"></i></a></li>
+                  <li><a href="#"><i className="icon-fa icon-logomark"></i></a></li>
             </ul>
           </div>
         </div>
