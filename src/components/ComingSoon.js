9182ea860ec6c38ef9c19e90c6721a6b73582654
Ben Murray <ben@mediacarry.com>
2022-02-13 22:57:52 +0000

    Update ComingSoon.js

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -79,9 +79,9 @@ export const ComingSoon = () => {
                 <a href='https://twitter.com/honeyznft' className='btn2'>Follow for pre-sale updates <span class="arrow"></span></a>
                 <div className="col mobile-show">
                   <ul className='social-links'>
-                    <li><a href-='https://twitter.com/honeyznft'><i className="icon-fa icon-twitter"></i></a></li>
-                    <li><a href-='https://discord.com/invite/pT52wbTFJ7'><i className="icon-fa icon-discord"></i></a></li>
-                    <li><a href-='https://www.instagram.com/honeyznft/'><i className="icon-fa icon-instagram"></i></a></li>
+                    <a href-='https://twitter.com/honeyznft'><li><i className="icon-fa icon-twitter"></i></li></a>
+                    <a href-='https://discord.com/invite/pT52wbTFJ7'><li><i className="icon-fa icon-discord"></i></li></a>
+                    <a href-='https://www.instagram.com/honeyznft/'><li><i className="icon-fa icon-instagram"></i></li></a>
                   </ul>
                 </div>
               </div>
