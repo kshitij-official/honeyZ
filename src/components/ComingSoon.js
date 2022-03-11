9a114d54f23b5f96304191848e544f9327105fc5
Karan <karansaddi2207@gmail.com>
2022-03-12 01:45:43 +0530

    minting

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -13,18 +13,63 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import Web3 from 'web3';
+import { CONTRACT_ADDRESS , CONTRACT_ABI } from '../config';
+
 export const ComingSoon = () => {
   const [width, setWidth] = useState(window.innerWidth);
+  const [accountCoonected, setAccountCoonected] = useState(false);
+
+  const web3 = new Web3(Web3.givenProvider || "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161");
+  const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
 
   function handleWindowSizeChange() {
     setWidth(window.innerWidth);
   }
+
+  //metamask integration
+  async function handleAccounts()
+  {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    //console.log(accounts[0]);
+    if(accounts[0])
+    {
+      setAccountCoonected(true);
+    }
+    const networkId = await window.ethereum.request({
+      method: "net_version",
+    });
+    if(networkId != 1)
+    {
+      alert("Please connect your wallet with Ethereum Mainnet");
+    }
+    //console.log(networkId);
+  }
+  
   useEffect(() => {
+    handleAccounts();
     window.addEventListener('resize', handleWindowSizeChange);
     return () => {
         window.removeEventListener('resize', handleWindowSizeChange);
     }
   }, []);
+
+  const openMetaMask = async () => {
+    //handleAccounts();
+    let accounts = await web3.eth.getAccounts();
+    let selectedAccountCustom = accounts[0];
+    let Cost = await contract.methods.cost().call();
+    let mintAmount = 1;
+    contract.methods.mint(mintAmount)
+      .send({ 
+            from: selectedAccountCustom ,
+            gasLimit: "300000",
+            value: Cost * mintAmount
+            //value: web3.utils.toWei((cost * amount).toString() , "ether")
+          });
+  }
 
   var settings = {
     dots: false,
