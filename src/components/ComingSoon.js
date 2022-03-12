1a055a3e308b79a8b7a4659ddc01ee2f0db51471
Ben Murray <ben@mediacarry.com>
2022-03-12 00:39:24 +0000

    limiting value to 1

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -79,9 +79,9 @@ export const ComingSoon = () => {
 		{
 			return alert("Please enter a number only");
 		}
-		if(amount.value > 5)
+		if(amount.value > 1)
 		{
-			return alert("Please mint less than 5 nfts at a time");
+			return alert("You can only mint 1 NFT at a time");
 		}
 
 		openMetaMask(amount.value);
